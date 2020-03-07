acdad6b9bb79d9ad70fcd710145b3296e28f8369
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-03-07 15:41:10 -0500

    polish resume page styles

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,6 +1,7 @@
 import Education from '../js/education';
+import React from 'react';
 
-let data = [new Education('Lambda School','Full-Stack Web & Computer Science', 'October 2019 - Feburary 2021', ['Front End: HTML, CSS, JavaScript', 'Back End: Node.js, SQL', 'Data Structures, Algorithms, Architecture', 'Git/GitHub', 'Python']),
-new Education('Spring Arbor University', 'Bachelor of Arts: Children’s Ministry', 'January 2011 - May 2013', ['Graduated with a 3.55 Cumulative GPA','Awarded Academic Honors'])]
+let data = [new Education('Lambda School','Full-Stack Web & Computer Science', 'October 2019 - Feburary 2021', ['Front End: HTML, CSS, JavaScript',<li>{'Back End: Node.js, SQL, Python'}</li>,<li>{'Data Structures, Algorithms, Architecture'}</li>,<li>{'Git/GitHub'}</li>]),
+new Education('Spring Arbor University', 'Bachelor of Arts: Children’s Ministry', 'January 2011 - May 2013', ['Graduated with a 3.55 Cumulative GPA',<li>{'Awarded Academic Honors'}</li>])]
 
 export default data;
