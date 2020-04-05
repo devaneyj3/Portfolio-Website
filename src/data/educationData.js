d27619dbdf050ae8f7e09a16ecbb030bcc660616
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-04 22:22:05 -0400

    modified some data

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,7 +1,7 @@
 import Education from '../js/education';
 import React from 'react';
 
-let data = [new Education('Lambda School','Full-Stack Web & Computer Science', 'October 2019 - Feburary 2021', ['Front End: HTML, CSS, JavaScript',<li>{'Back End: Node.js, SQL, Python'}</li>,<li>{'Data Structures, Algorithms, Architecture'}</li>,<li>{'Git/GitHub'}</li>]),
+let data = [new Education('Lambda School','Full-Stack Web & Computer Science', 'October 2019 - June 2021', ['Front End: HTML, CSS, JavaScript, React, Redux',<li>{'Back End: Node.js, SQL, Python'}</li>,<li>{'Data Structures, Algorithms, Architecture'}</li>,<li>{'Git/GitHub'}</li>]),
 new Education('Spring Arbor University', 'Bachelor of Arts: Children’s Ministry', 'January 2011 - May 2013', ['Graduated with a 3.55 Cumulative GPA',<li>{'Awarded Academic Honors'}</li>])]
 
 export default data;
