cb264190b2bf95f9499a4e8db29830d9dbb70aee
Jordan <jordandevaney28@gmail.com>
2021-01-26 13:05:18 -0500

    added backgound photo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import me from '../../images/me.jpg';
+import React, { useState } from "react";
+import me from "../../images/me.png";
+import "./Header.scss";
 
 const Header = () => {
-    return (
-        <section className='home'>
-                <div className='header'>
-                    <h1>Full Stack Web Developer</h1>
-                    <p>I love coding projects that have giant influence.</p>
-            <img src={me} alt='me'></img>
-                </div>
-        </section>
-    )
-}
+  const [aboutText, setAboutText] = useState("Hello.");
+  const [workText, setWorkText] = useState("I am");
+  const [contactText, setContactText] = useState("Jordan");
+  return (
+    <section className="home">
+      <section className="links">
+        <a href="/about">
+          <h1
+            onMouseEnter={() => setAboutText("About")}
+            onMouseLeave={() => setAboutText("Hello.")}
+          >
+            {aboutText}
+          </h1>
+        </a>
+        <a href="/projects">
+          <h1
+            onMouseEnter={() => setWorkText("Work")}
+            onMouseLeave={() => setWorkText("I am")}
+          >
+            {workText}
+          </h1>
+        </a>
+        <a href="/contact">
+          <h1
+            onMouseEnter={() => setContactText("Contact")}
+            onMouseLeave={() => setContactText("Jordan")}
+          >
+            {contactText}
+          </h1>
+        </a>
+      </section>
+      <div
+        className="bg-photo"
+        style={{
+          backgroundImage: `url(${me})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    </section>
+  );
+};
 
 export default Header;
--- src/container/App.js
+++ src/container/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import Portfolio from "../Pages/PortfolioPage/Portfolio";
+import Header from "../components/Header/Header";
 import Contact from "../Pages/ContactPage/Contact";
 import Resume from "../Pages/ResumePage/Resume";
-import { Route, Redirect } from "react-router-dom";
+import Work from "../components/Work/Work.jsx";
+import Projects from "../components/Projects/Projects";
+import About from "../components/About/About";
+import { Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./reset.scss";
 import "./media_queries.scss";
@@ -10,10 +13,12 @@ import "./media_queries.scss";
 function App() {
   return (
     <div>
-      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/" component={Header} />
+      <Route exact path="/work" component={Work} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/resume" component={Resume} />
-      <Redirect from="/" to="/portfolio" />
     </div>
   );
 }
