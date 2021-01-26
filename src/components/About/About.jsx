1e467caae5868105100ad887ca951c6c8cb4aa20
Jordan <jordandevaney28@gmail.com>
2021-01-26 13:31:23 -0500

    make about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './About.scss'
 
 const About = () => {
     return (
         <section className='About'>
-          <div>
-              <h1>Hi, I'm Jordan. Nice to meet you.</h1>
-              <p>I started beginning my coding journey in 2017. Since then I have done numerous projects that are highlighted below. </p>
+          <div className='about-header'>
+            <h1>I love building innovative and creative Full Stack Applications</h1>
           </div>
+          <section className="about-body">
+              <h1>About</h1>
+              <p>I started beginning my coding journey in 2017. Since then I have done numerous projects that are highlighted below. </p>
+          </section>
         </section>
     )
 }
