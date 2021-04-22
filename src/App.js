bd1375f9f4b53f5048916571fd44b5c29cfd5a1f
Jordan <jordandevaney28@gmail.com>
2021-04-22 19:33:13 -0400

    convert to es6 and functional components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import $ from "jquery";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { data } from "./resumeData";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
@@ -9,49 +8,23 @@ import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      resumeData: {},
-    };
+const App = () => {
+  const [resumeData, setResumeData] = useState(data);
 
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
+  useEffect(() => {
+    setResumeData(data);
+  }, []);
 
-  getResumeData() {
-    $.ajax({
-      url: "/resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      },
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Header data={resumeData} />
+      <About data={resumeData} />
+      <Portfolio data={resumeData} />
+      <Resume data={resumeData} />
+      <Contact data={resumeData} />
+      <Footer data={resumeData} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,140 @@
+export const data = {
+  name: "Jordan Devaney",
+  occupation: "Full Stack Web Developer",
+  description:
+    "I have a strong desire to be a productive member of a team environment that fosters learning, growth, and develops a great product for the user",
+  image: "Me.jpg",
+  bio:
+    "I am a Full Stack Web Developer passionate about solving complex problems and making great websites that will benefit the client. I am determined to serve you with my skills and meet your need.",
+  message: "Can I help you on your next project? Let's chat.",
+  email: "jordandevaney28@gmail.com",
+  phone: "810-220-9256",
+  address: {
+    street: "351 Emerald Ct E",
+    city: "Whitmore Lake",
+    state: "Michigan",
+    zip: 48189,
+  },
+  website: "http://www.timbakerdev.com",
+  resumedownload:
+    "https://docs.google.com/document/d/1s6z0l5ZkmVUJUtrztj5EwSZvry8BQm2dFyws4GhKzw0/edit?usp=sharing",
+  social: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/jordandevaney/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/devaneyj3",
+      className: "fa fa-github",
+    },
+  ],
+  resume: {
+    skillmessage:
+      "I have developed superb in demand skills through these awesome college. Also, from various Udemy and other Massive Open Online Courses (MOOCs)",
+    education: [
+      {
+        school: "Lambda School",
+        degree: "Full Stack Web Development and Computer Science",
+        graduated: "January 2021",
+        description:
+          "Worked on cross functional teams and building products with React, Redux, Context, Node, Python, and SQL",
+      },
+      {
+        school: "Spring Arbor University",
+        degree: "Bachelor of Arts: Children’s Ministry",
+        graduated: "May 2013",
+      },
+    ],
+    work: [
+      {
+        company: "thesoftwareaccelerator.com",
+        title: "Founder/Content Creator",
+        years: "December 2020 - Current",
+        description:
+          "Maintained WordPress website. Engaged in professional development video casts to polish and learn new skills. Incorporated design elements to enhance UX Research article ideas and create content centered around web development. Utilized Copywriting strategies and affilate marketing to increase profits. Utilized SEO optimization to rank on Google and other search engines",
+      },
+      {
+        company: "DoorDash",
+        title: "Delivery Driver",
+        years: "January 2019 - Current",
+        description:
+          "Comleted over 2000 deliveries with a 4.96 rating out of 5.Operated fast-paced delivery service throughout the Brighton market. Planned and strategized more efficient routes to increase speed of service. Received compliments for my timely service",
+      },
+    ],
+    skills: [
+      {
+        name: "JavaScript",
+        level: "70%",
+      },
+      {
+        name: "Python",
+        level: "55%",
+      },
+      {
+        name: "ReactJs",
+        level: "80%",
+      },
+      {
+        name: "SCSS",
+        level: "60%",
+      },
+      {
+        name: "HTML5",
+        level: "80%",
+      },
+      {
+        name: "SQL",
+        level: "70%",
+      },
+      {
+        name: "Node",
+        level: "80%",
+      },
+    ],
+  },
+  portfolio: {
+    projects: [
+      {
+        title: "Anywhere Fitness",
+        category:
+          "A fitness management app where instructors can post classes and students can sign up",
+        image: "fitness.jpg",
+        url: "https://inspiring-jones-a91418.netlify.app/",
+      },
+      {
+        title: "Simon Game",
+        category: "Replica of the old Simon Game",
+        image: "simon-game.png",
+        url: "https://devaneyj3.github.io/simon_game/",
+      },
+      {
+        title: "To Do List",
+        category:
+          "Utilized Redux with React to make a todo app. Constructed API endpoints using Node and database schema using Knex",
+        image: "todo.jpg",
+        url: "https://quizzical-joliot-698fe7.netlify.app/",
+      },
+      {
+        title: "Game of Life",
+        category:
+          "This project was a part of one of my build weeks at Lambda School. This was done using HTML Canvas, Vanilla JS and Bootstrap",
+        image: "life.png",
+        url: "https://game-of-lif-jordan.netlify.app/",
+      },
+      {
+        title: "Shopping Cart",
+        category: "Used React and Context API to build a basic shopping cart.",
+        image: "cart.png",
+        url: "https://react-shopping-cart.devaneyj3.now.sh/cart",
+      },
+      {
+        title: "Budget App",
+        category: "Utilized React and Redux to make a buget and checkbook",
+        image: "budget.png",
+        url: "https://dazzling-kirch-79dfd6.netlify.app/",
+      },
+    ],
+  },
+};
