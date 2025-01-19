6cfec0ad9304dad1d2cc4c5a78b7f42ac241a487
jordandevaney <jordandevaney28@gmail.com>
2025-01-19 13:46:45 -0500

    add a title to my site

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" title="Portfolio Website">
 			<head>
+				<title>Portfolio Website</title>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta
