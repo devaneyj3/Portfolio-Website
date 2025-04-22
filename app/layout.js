ae3aa48b580ba2188d8e982432ff33cdccd8237a
devaneyj3 <jordandevaney28@gmail.com>
2025-04-22 15:40:47 -0400

    changed image in manifest

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import "./globals.css";
 
@@ -62,21 +61,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-	React.useEffect(() => {
-		if ("serviceWorker" in navigator) {
-			navigator.serviceWorker
-				.register("/service-worker.js")
-				.then((registration) => {
-					console.log(
-						"Service Worker registered with scope:",
-						registration.scope
-					);
-				})
-				.catch((error) => {
-					console.error("Service Worker registration failed:", error);
-				});
-		}
-	}, []);
 	return (
 		<html lang="en" title="Portfolio Website">
 			<head>
